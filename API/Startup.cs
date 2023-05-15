2e2568976a59eaec46c8162810aafee3c84b7a4a
Loick <loickngoual91@gmail.com>
2023-05-15 15:57:56 +0100

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,10 +22,18 @@ namespace API
         {
             //services.AddScoped<IProductRepository,ProductRepository>();
             services.AddControllers();
-            services.AddApplicationServices();
-            services.AddSwaggerDocumentation();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -50,6 +58,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
